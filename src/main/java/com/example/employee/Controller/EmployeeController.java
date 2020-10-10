8cae94449746269d7294dd38235e4202e5df7b4d
Kailash Mamidi <kmamidi@opentext.com>
2020-10-10 13:05:39 -0400

    created employee project

---
+++ src/main/java/com/example/employee/Controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.example.employee.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    @GetMapping("/get")
+    public String getEmployeeName(){
+        return "kailash";
+    }
+}
